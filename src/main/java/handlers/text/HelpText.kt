57bed1e8399b8b693b47d89a4bb364cb14170510
Georgiy Levchenko <55250309+fan-mag@users.noreply.github.com>
2020-03-30 19:54:07 +0300

    HelpText refactored

--- src/main/java/handlers/text/HelpText.kt
+++ src/main/java/handlers/text/HelpText.kt
@@ -3,7 +3,6 @@ package handlers.text
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage
 import properties.RoomProperty.MAX_ROOM_CAPACITY
 import properties.RoomProperty.MIN_ROOM_CAPACITY
-import java.util.*
 
 class HelpText : TextHandler {
 
@@ -17,13 +16,6 @@ class HelpText : TextHandler {
     override fun handle(): List<SendMessage> {
         val sb = StringBuilder()
         Commands.values().forEach { sb.append(it.prefix).append(" - ").append(it.description).append(System.lineSeparator()) }
-        val helpMessage = SendMessage().setText(sb.toString())
-        return listOf(helpMessage)
-    }
-
-    private fun <T> listOf(t: T): List<T> {
-        val list: MutableList<T> = ArrayList()
-        list.add(t)
-        return list
+        return listOf(SendMessage().setText(sb.toString()))
     }
 }
